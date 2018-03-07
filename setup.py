278ab7b675efd86d7fe00c327542c104064a8da0
keuv-grvl <k.gravouil@gmail.com>
2018-03-07 11:52:28 +0100

    add requirements + classifiers

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from setuptools import setup
-import fennec
+from fennec import __version__
+
+requirements = open('requirements.txt').read().splitlines()
 
 setup(name='fennec',
-      version=fennec.__version__,
+      version=__version__,
       description='DNA sequence modeling for machine learning',
       url='https://github.com/keuv-grvl/fennec',
       author='Kévin Gravouil',
       author_email='kevin.gravouil@uca.fr',
       license='MIT',
       packages=['fennec'],
-      install_requires=[
-          'scikit-bio',
-          'scikit-learn',
-          'bcbio-gff'
-      ],
-      zip_safe=False)
+      install_requires=requirements,
+      #see: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Intended Audience :: Science/Research'
+        ]
+      )
