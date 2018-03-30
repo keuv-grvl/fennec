f5c00fc7ff4f3be337a2264542213b2d6f326284
keuv-grvl <k.gravouil@gmail.com>
2018-03-30 20:59:40 +0200

    init SequenceAbundanceModel

--- fennec/__init__.py
+++ fennec/__init__.py
@@ -5,17 +5,18 @@ from ._models import (
         MaskedKmerModel,
         InterNucleotideDistanceModel,
         CodingDensityModel,
-        Contig2VecModel
+        Contig2VecModel,
+        SequenceAbundanceModel
     )
 
 from ._sentence2vec.sentence2vec import Word, Sentence, get_word_frequency, sentence_to_vec
 
-
-__version__ = '0.1-dev'
+__version__ = 'dev.0.1'
 __all__ = [
         'DNASequenceBank',
         'MaskedKmerModel',
         'InterNucleotideDistanceModel',
         'CodingDensityModel',
-        'Contig2VecModel'
+        'Contig2VecModel',
+        'SequenceAbundanceModel'
     ]
